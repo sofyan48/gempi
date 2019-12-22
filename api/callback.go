c992c3f853d890e70dd7f0bbb4b82b8d034557c4
Sofyan <sofyan.saputra@ralali.com>
2019-12-23 00:14:37 +0700

    Add: Calback in Function

---
+++ api/callback.go
@@ -0,0 +1,54 @@
+package api
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/sofyan48/gempi/entity"
+	"github.com/sofyan48/gempi/libs"
+)
+
+// Callback ...
+type Callback struct{}
+
+// GetCallbackFunction ...
+func GetCallbackFunction() *Callback {
+	return &Callback{}
+}
+
+// Flush messages
+// @msg: *sqs.Message
+// @data: *entity.StateFullModels
+func (clb *Callback) Flush(cl *entity.NewClient, msg *sqs.Message, data *entity.StateFullModels) {
+	dataSend := &entity.StateFullModels{}
+	dataSend.Status = "done"
+	dataSend.Body = data.Body
+	dataSend.Topic = data.Topic
+	body, _ := json.Marshal(dataSend)
+	awsLibs := &libs.Aws{}
+
+	msgBroker := awsLibs.GetMessagesInput()
+	msgBroker.QueueUrl = aws.String(cl.Configs.PathURL + "/broker")
+	msgBroker.DelaySeconds = aws.Int64(3)
+	msgBroker.MessageBody = aws.String(string(body))
+	_, err := awsLibs.Send(cl.Sessions, msgBroker)
+	if err != nil {
+		log.Println("Error : ", err)
+	}
+	_, err = awsLibs.Delete(cl.Sessions, msg, cl.Configs.PathURL+"/broker")
+	log.Println("Status Done : ", *msg.Body)
+}
+
+// Deleted messages
+// @msg: *sqs.Message
+// @data: *entity.StateFullModels
+func (clb *Callback) Deleted(cl *entity.NewClient, msg *sqs.Message, data *entity.StateFullModels) {
+	awsLibs := &libs.Aws{}
+	_, err := awsLibs.Delete(cl.Sessions, msg, cl.Configs.PathURL+"/broker")
+	if err != nil {
+		log.Println("Error : ", err)
+	}
+	log.Println("Status Done : ", *msg.Body)
+}
